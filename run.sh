6611ef2cfc5dad29679e428fe997adfa514e5dee
awan hitam <165126976+awanhtm@users.noreply.github.com>
2024-03-27 12:42:07 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-4c4dbd84-fa07-41f8-a7e4-5ca31af54b0d/)
 
 
 echo "$curl_output" >> Update.tsx
